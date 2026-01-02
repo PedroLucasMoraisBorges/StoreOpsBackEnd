d0542ec17313b20420960ce7bb17245fb66d1ba3
Pedro Lucas <pedrolucasdemoraisborges@gmail.com>
2026-01-02 11:03:34 -0300

    Initial commit

---
+++ src/main/java/com/StoreOpsBackEnd/StoreOpsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.StoreOpsBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreOpsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreOpsBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StoreOpsBackEnd/StoreOpsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StoreOpsBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreOpsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
